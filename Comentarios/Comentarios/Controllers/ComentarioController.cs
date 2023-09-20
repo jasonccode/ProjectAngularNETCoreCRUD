42fa4383fbb2ca1bfdf7bd75a301ea1019c77f5d
Jason C <grousemusic@gmail.com>
2023-09-19 19:00:22 -0500

    Get and Post

--- Comentarios/Comentarios/Controllers/ComentarioController.cs
+++ Comentarios/Comentarios/Controllers/ComentarioController.cs
@@ -2,46 +2,90 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Comentarios.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Comentarios.Controllers
 {
-    [Route("api/[controller]")]
-    public class ComentarioController : Controller
+  [Route("api/[controller]")]
+  public class ComentarioController : Controller
+  {
+    private readonly AplicationDbContext _context;
+
+    public ComentarioController(AplicationDbContext context)
     {
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+      _context = context;
     }
+
+
+
+    // GET: api/values
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+      try
+      {
+        var listComentarios = await _context.Comentario.ToListAsync();
+        return Ok(listComentarios);
+      }
+      catch (Exception ex)
+      {
+        return BadRequest(ex.Message);
+      }
+    }
+
+    // GET api/values/5
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(int id)
+    {
+      try
+      {
+        var comentario = await _context.Comentario.FindAsync(id);
+
+        if (comentario == null)
+        {
+          return NotFound();
+        }
+
+        return Ok(comentario);
+      }
+      catch (Exception ex)
+      {
+        return BadRequest(ex.Message);
+      }
+    }
+
+    // POST api/values
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] Comentario comentario)
+    {
+      try
+      {
+        _context.Add(comentario);
+        await _context.SaveChangesAsync();
+
+        return Ok(comentario);
+      }
+      catch (Exception ex)
+      {
+        return BadRequest(ex.Message);
+      }
+    }
+
+    // PUT api/values/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/values/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+  }
 }
 
